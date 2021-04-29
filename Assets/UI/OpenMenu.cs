c1a3484293ccccf4da0a663a5b6514a8dcb7eefe
lotous21 <66188971+lotous21@users.noreply.github.com>
2021-04-29 13:10:17 +0300

    UI Version 0.1
    *Open Scene Added
    *New Game Panel (Choose Spec & Avatar)
    *Settings Panel (Audio & Resolution)
    *HP Bar Added

---
+++ Assets/UI/OpenMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenMenu : MonoBehaviour
+{
+
+    public GameObject ButtonsPanel;
+    public GameObject NewGameMenu;
+    public GameObject SettingsMenu;
+
+    public GameObject SpecSelector;
+    public GameObject AvatarSelector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonsPanel.SetActive(true);
+        NewGameMenu.SetActive(false);
+        SettingsMenu.SetActive(false);
+        SpecSelector.SetActive(true);
+        AvatarSelector.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenNewGame()
+    {
+        ButtonsPanel.SetActive(false);
+        NewGameMenu.SetActive(true);
+    }
+
+    public void ExitNewGame()
+    {
+        ButtonsPanel.SetActive(true);
+        NewGameMenu.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        ButtonsPanel.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+    
+    public void ExitSettings()
+    {
+        ButtonsPanel.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenAvatarSelector()
+    {
+        SpecSelector.SetActive(false);
+        AvatarSelector.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
